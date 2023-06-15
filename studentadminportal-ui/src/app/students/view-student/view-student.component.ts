710d04130be56f8a4d8ce6a80471318052b5e33a
alecchoi <alec.choi8@gmail.com>
2023-06-14 21:17:19 -0400

    End of Section 8

--- studentadminportal-ui/src/app/students/view-student/view-student.component.ts
+++ studentadminportal-ui/src/app/students/view-student/view-student.component.ts
@@ -35,6 +35,7 @@ export class ViewStudentComponent implements OnInit {
 
   isNewStudent = false;
   header = '';
+  displayProfileImageUrl = '';
 
   genderList: Gender[] = [];
 
@@ -56,6 +57,7 @@ export class ViewStudentComponent implements OnInit {
           // -> newStudent Functionality
           this.isNewStudent = true;
           this.header = 'Add New Student';
+          this.setImage();
         }
 
         else{
@@ -66,6 +68,10 @@ export class ViewStudentComponent implements OnInit {
           .subscribe(
             (successResponse) =>{
              this.student = successResponse;
+             this.setImage();
+            },
+            (errorResponse)=>{
+              this.setImage();
             }
           );
         }
@@ -136,4 +142,38 @@ export class ViewStudentComponent implements OnInit {
       }
     );
   }
+
+  uploadImage(event: any): void{
+    if(this.studentId){
+      const file: File = event.target.files[0];
+      this.studentService.uploadImage(this.student.id, file)
+      .subscribe(
+        (successResponse)=>{
+          this.student.profileImageUrl = successResponse;
+          this.setImage();
+        //show a notification
+        this.snackbar.open('Profile Image Updated', undefined,{
+          duration: 2000
+        });
+
+        },
+        (errorResponse)=>{
+
+        }
+      );
+
+    }
+  }
+
+  private setImage(): void{
+    if(this.student.profileImageUrl){
+      this.displayProfileImageUrl = this.studentService.getImagePath(this.student.profileImageUrl);
+
+    }else{
+      //Display a default
+      this.displayProfileImageUrl = '/assets/user.png';
+    }
+  }
+
+
 }
